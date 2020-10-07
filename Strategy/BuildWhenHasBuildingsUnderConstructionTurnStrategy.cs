037229cb467427e527470aae92cf448864820acd
peter.aslund@me <peter@peteraslund.me>
2020-10-07 23:58:10 +0200

    AdjustEnergy if needed building constructed

--- Strategy/BuildWhenHasBuildingsUnderConstructionTurnStrategy.cs
+++ Strategy/BuildWhenHasBuildingsUnderConstructionTurnStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using DotNet.models;
 
 namespace DotNet.Strategy
 {
@@ -7,6 +8,10 @@ namespace DotNet.Strategy
 		public BuildWhenHasBuildingsUnderConstructionTurnStrategy(TurnStrategyBase parent = null) : base(parent)
 		{
 		}
+
+		public bool AdjustHeatOnConstructed { get; set; } = true;
+
+		public double AllowedDiffMargin { get; set; } = 0.5;
 
 		protected override bool TryExecuteTurn(Randomizer randomizer, GameLayer gameLayer, GameState state)
 		{
@@ -23,9 +28,45 @@ namespace DotNet.Strategy
 			{
 				var position = building.Position;
 				gameLayer.ExecuteAction(GameActions.Build, position);
+
+				building = state.GetCompletedBuildings().FirstOrDefault(x => x.Position.ToString() == position.ToString());
+
+				if (building is BuiltResidenceBuilding residence)
+				{
+					if (AdjustHeatOnConstructed)
+					{
+						var blueprint = gameLayer.GetResidenceBlueprint(building.BuildingName);
+
+						var energy = blueprint.BaseEnergyNeed + (residence.Temperature - state.CurrentTemp)
+							* blueprint.Emissivity / 1 - 0.5 - residence.CurrentPop * 0.04;
+
+						if (IsBetween(energy,
+							residence.RequestedEnergyIn - AllowedDiffMargin,
+							residence.RequestedEnergyIn + AllowedDiffMargin))
+						{
+							// current energy setting is sufficient
+						}
+						else
+						{
+							// adjust energy as the first action after completing building (will take one turn)
+							gameLayer.AdjustEnergy(building.Position, energy);
+						}
+					}
+				}
+				else
+				{
+
+				}
 				return true;
 			}
 			return false;
+		}
+
+		public static bool IsBetween(double num, double lower, double upper, bool inclusive = false)
+		{
+			return inclusive
+				? lower <= num && num <= upper
+				: lower < num && num < upper;
 		}
 	}
 }
