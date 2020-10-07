1b382986d602db627bec9f4b7db0e4a86a0da1ae
peter.aslund@me <peter@peteraslund.me>
2020-10-07 11:40:23 +0200

    Maintenance improvements
    - Don't do maintenance cant afford it
    - Prioritize maintenance

--- Strategy/MaintenanceWhenBuildingIsGettingDamagedTurnStrategy.cs
+++ Strategy/MaintenanceWhenBuildingIsGettingDamagedTurnStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using DotNet.models;
 
 namespace DotNet.Strategy
 {
@@ -10,24 +11,43 @@ namespace DotNet.Strategy
 
 		public int ThresholdHealth { get; set; } = 50;
 
+		public bool PrioritizeWeakest { get; set; } = false;
+
 		protected override bool TryExecuteTurn(Randomizer randomizer, GameLayer gameLayer, GameState state)
 		{
-			var damagedBuildings = state.ResidenceBuildings.Where(x => x.Health < ThresholdHealth).ToArray();
+			var damagedBuildings = state.ResidenceBuildings.Where(x => x.Health < ThresholdHealth)
+				.OrderBy(x => x.Health)
+				.ToArray();
+			if (!damagedBuildings.Any())
+				return false;
 
-			// Randomly choose building
-			//var building = damagedBuildings.ElementAtOrDefault(randomizer.Random.Next(0, damagedBuildings.Length));
-
-			// Choose the one with least health
-			var building = damagedBuildings.OrderBy(x => x.Health).FirstOrDefault();
-
-
-			if (building != null)
+			var affordedBuildings = damagedBuildings.Where(x =>
 			{
-				var position = building.Position;
-				gameLayer.ExecuteAction(GameActions.Maintenance, position);
-				return true;
+				var blueprint = state.AvailableResidenceBuildings.Single(p => p.BuildingName == x.BuildingName);
+				var afforded = blueprint.MaintenanceCost < state.Funds;
+				return afforded;
+			}).ToArray();
+
+			BuiltResidenceBuilding building;
+			if (PrioritizeWeakest)
+			{
+				building = damagedBuildings.First();
+				if (!affordedBuildings.Contains(building))
+				{
+					// Can't afford it, then skip hole strategy to save money for next time
+					return false;
+				}
 			}
-			return false;
+			else
+			{
+				// Can afford maintenance on the weakest building
+				building = affordedBuildings.First();
+			}
+
+			
+			var position = building.Position;
+			gameLayer.ExecuteAction(GameActions.Maintenance, position);
+			return true;
 		}
 	}
 }
