10ab551c60ef66965f7e68f63bc82f4ef84f4efe
peter.aslund@me <peter@peteraslund.me>
2020-10-08 12:42:55 +0200

    Updated to log level formatting when InMemoryLogger

--- DotNet/Logging/InMemoryLogger.cs
+++ DotNet/Logging/InMemoryLogger.cs
@@ -21,8 +21,36 @@ namespace DotNet.Logging
 		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
 		{
 			var msg = formatter.Invoke(state, exception);
-			var formattedLine = $"{logLevel}: {_categoryName}[{eventId.Id}]: {msg}";
+			var lvl = GetLogLevelAsShortString(logLevel);
+			var formattedLine = $"{lvl}: {_categoryName}[{eventId.Id}]: {msg}";
 			StringBuilder.AppendLine(formattedLine);
+		}
+
+		public string GetLogLevelAsShortString(LogLevel logLevel)
+		{
+			switch (logLevel)
+			{
+				case LogLevel.Critical:
+					return "FTL";
+
+				case LogLevel.Error:
+					return "ERR";
+
+				case LogLevel.Warning:
+					return "WRN";
+
+				case LogLevel.Information:
+					return "INF";
+
+				case LogLevel.Debug:
+					return "DBG";
+
+				case LogLevel.Trace:
+					return "TRC";
+
+				default:
+					return logLevel.ToString();
+			}
 		}
 
 		public bool IsEnabled(LogLevel logLevel)
